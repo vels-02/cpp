b08917780b4f32d9efa31ba9c31efdad2c890b72
vels-02 <37198348+vels-02@users.noreply.github.com>
2020-10-15 23:36:35 -0700

    Update firstandlast_occurance.cpp

--- firstandlast_occurance.cpp
+++ firstandlast_occurance.cpp
@@ -49,9 +49,44 @@ int BinarySearch(int arr[], int n, int x,bool firstoccur)
     
 }
 
+int circularArrSearch(int arr[], int n,int x)
+{
+    int low = 0, high = n-1; 
+    
+    while(low <= high)
+    {
+        int mid = (low+high) / 2;
+        
+        //find the sorted half-- because we can to binary search
+        if( x == arr[mid] ) return mid;
+        
+        if(arr[mid] <= arr[high] )
+        {
+            if(x > arr[mid] && x <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+                
+        }
+        else
+        {
+            if(arr[low] <= x && x < arr[mid]) 
+            {
+                high = mid-1;
+            }
+            else
+                low = mid + 1;
+        }   
+    }
+    
+    return -1;
+}
+
 int main()
 {
-   int arr[]  = {2, 4, 5, 5, 5, 5, 5, 5, 10, 10, 10, 18, 20};
+      int arr[]  = {2, 4, 5, 5, 5, 5, 5, 5, 10, 10, 10, 18, 20};
     
     int len = sizeof(arr) / sizeof(arr[0]);
     cout << "first index of 5 is " << BinarySearch(arr, len, 5, true) << endl;
@@ -66,6 +101,14 @@ int main()
          
          cout << "Count is : " << (lastindex-firstindex) + 1 << endl;
     }
+    
+    
+    //find an element in ciruclary sorted array
+    int arr1[]  = {12,14,18,21, 3, 6,8 ,9 };
+    len = sizeof(arr1) / sizeof(arr1[0]);
+    
+    cout << circularArrSearch(arr1, len, 21);
+
 
     return 0;
 }
